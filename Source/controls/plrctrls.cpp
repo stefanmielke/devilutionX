9decd6b472b1732982b08e8b96c72de3ba4a5491
Stefan Mielke <stefanmielke@fastmail.com>
2021-05-05 21:04:33 -0300

    fix belt item movement

--- Source/controls/plrctrls.cpp
+++ Source/controls/plrctrls.cpp
@@ -628,7 +628,11 @@ void InvMove(AxisDirection dir)
 				}
 				mousePos.x = InvRect[slot].X + RIGHT_PANEL;
 				mousePos.y = InvRect[slot].Y;
-			} else if (plr[myplr].HoldItem._itype == ITYPE_RING) {
+			} else if (slot > SLOTXY_BELT_FIRST && slot <= SLOTXY_BELT_LAST) {
+                slot -= 1;
+                mousePos.x = InvRect[slot].X + PANEL_LEFT;
+                mousePos.y = InvRect[slot].Y + PANEL_TOP;
+            } else if (plr[myplr].HoldItem._itype == ITYPE_RING) {
                 slot = SLOTXY_RING_LEFT;
 				mousePos = InvGetSlotCoord(INVLOC_RING_LEFT);
 			} else if (plr[myplr].HoldItem.isWeapon() || plr[myplr].HoldItem.isShield()) {
@@ -661,7 +665,7 @@ void InvMove(AxisDirection dir)
 				}
 				mousePos.x = InvRect[slot].X + RIGHT_PANEL;
 				mousePos.y = InvRect[slot].Y;
-			} else if (slot > SLOTXY_BELT_FIRST && slot <= SLOTXY_BELT_LAST) { // belt
+			} else if (slot > SLOTXY_BELT_FIRST && slot <= SLOTXY_BELT_LAST) {
 				slot -= 1;
 				mousePos.x = InvRect[slot].X + PANEL_LEFT;
 				mousePos.y = InvRect[slot].Y + PANEL_TOP;
@@ -680,7 +684,11 @@ void InvMove(AxisDirection dir)
 				}
 				mousePos.x = InvRect[slot].X + RIGHT_PANEL;
 				mousePos.y = InvRect[slot].Y;
-			} else if (plr[myplr].HoldItem._itype == ITYPE_RING) {
+			} else if (slot >= SLOTXY_BELT_FIRST && slot < SLOTXY_BELT_LAST) {
+                slot += 1;
+                mousePos.x = InvRect[slot].X + PANEL_LEFT;
+                mousePos.y = InvRect[slot].Y + PANEL_TOP;
+            } else if (plr[myplr].HoldItem._itype == ITYPE_RING) {
                 slot = SLOTXY_RING_RIGHT;
 				mousePos = InvGetSlotCoord(INVLOC_RING_RIGHT);
 			} else if (plr[myplr].HoldItem.isWeapon() || plr[myplr].HoldItem.isShield()) {
@@ -799,8 +807,8 @@ void InvMove(AxisDirection dir)
 				mousePos.x = InvRect[slot].X + RIGHT_PANEL;
 				mousePos.y = InvRect[slot].Y;
 				SDL_Log("mouse: {%d, %d}; %d", mousePos.x, mousePos.y, InvRect[slot].Y);
-			} else if (slot <= (SLOTXY_BELT_LAST - 8) && plr[myplr].HoldItem._itype == ITYPE_MISC) {
-				slot += 8;
+			} else if (slot <= (SLOTXY_BELT_LAST - 10) && plr[myplr].HoldItem._itype == ITYPE_MISC && icursW28 == 1 && icursH28 == 1) { // forcing only 1x1 misc items
+				slot += 10;
 				mousePos.x = InvRect[slot].X + PANEL_LEFT;
 				mousePos.y = InvRect[slot].Y + PANEL_TOP;
 			}
